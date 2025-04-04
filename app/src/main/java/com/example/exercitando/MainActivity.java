750b247e48b0a47f69470276cdd03e201b0c4a38
otavi <otaviogandhi@gmail.com>
2025-04-03 21:49:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/exercitando/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.exercitando;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    String [] frases = {
+            "Primeira Frase",
+            "Segunda Frase",
+            "Terceira Frase",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+        public void gerarFrase(View view){
+
+        }
+        public void exibirTodas(View view){
+            for (String frase : frases) {
+                System.out.println(frase);
+            }
+        }
+    }
